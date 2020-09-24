6ed6b58e078fd3c310db1a5bb8d170922274435a
CampoDiFiori <dudkopiotr@gmail.com>
2020-09-24 18:28:44 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <memory>
+#include <chrono>
+#include <thread>
+#include <iostream>
+#include "video_reader.hpp"
+
+#define SDL_MAIN_HANDLED
+extern "C" {
+#include <SDL2/SDL.h>
+#include <libavformat/avformat.h>
+#include <libavcodec/avcodec.h>
+#include <libswscale/swscale.h>
+}
+
+#define SCREEN_WIDTH 960
+#define SCREEN_HEIGHT 540
+
+int main() {
+
+	VideoReader video_reader_state("C:\\Users\\dudko\\OneDrive\\Desktop\\rotating_globe.mp4");
+
+	if (SDL_Init(SDL_INIT_VIDEO) == -1) {
+		printf("SDL could not initialize: %s\n", SDL_GetError());
+		return 1;
+	}
+
+	const auto sdl_window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+	if (!sdl_window) {
+		fprintf(stderr, "Couldn't initialize the SDL window: %s\n", SDL_GetError());
+		return 1;
+	}
+
+	const auto rgb_channel_count = 4;
+
+	const auto sdl_window_format = SDL_GetWindowSurface(sdl_window)->format;
+
+	uint8_t* frame_data = nullptr;
+
+	SDL_Rect stretchRect;
+	stretchRect.x = 0;
+	stretchRect.y = 0;
+	stretchRect.w = SCREEN_WIDTH;
+	stretchRect.h = SCREEN_HEIGHT;
+
+	auto quit = false;
+	SDL_Event e;
+
+	std::chrono::steady_clock::time_point frame_begin_time, frame_end_time;
+	std::chrono::microseconds duration_since_start;
+
+	while (!quit) {
+
+		// frame_begin_time = std::chrono::steady_clock::now();
+
+		frame_data = video_reader_state.read_single_frame();
+
+		if (!frame_data) {
+			fprintf(stderr, "Couldn't load the frame");
+			return 1;
+		}
+
+		auto sdl_frame_surface = SDL_CreateRGBSurfaceFrom(
+			(void*)frame_data,
+			video_reader_state.width,
+			video_reader_state.height,
+			rgb_channel_count * 8,          // bits per pixel = 24
+			video_reader_state.width * rgb_channel_count,  	// pitch
+			0x0000FF,              			// red mask
+			0x00FF00,              			// green mask
+			0xFF0000,              			// blue mask
+			0
+		);
+
+		if (!sdl_frame_surface) {
+			fprintf(stderr, "Couldn't push frame pixels: %s\n", SDL_GetError());
+			return 1;
+		}
+
+		if (SDL_BlitScaled(sdl_frame_surface, nullptr, SDL_GetWindowSurface(sdl_window), &stretchRect) < 0) {
+			fprintf(stderr, "Couldn't blit the frame surface: %s\n", SDL_GetError());
+			return 1;
+		}
+
+		SDL_UpdateWindowSurface(sdl_window);
+
+		SDL_FreeSurface(sdl_frame_surface);
+
+		frame_end_time = std::chrono::steady_clock::now();
+
+		duration_since_start = std::chrono::duration_cast<std::chrono::microseconds>(frame_end_time - video_reader_state.first_frame_begin_time);
+
+		auto sleep_time = std::chrono::microseconds(video_reader_state.frame_pst_microsec) - duration_since_start;
+		std::this_thread::sleep_for(sleep_time);
+
+		printf("%d\n", sleep_time);
+
+		if (SDL_PollEvent(&e) != 0) {
+			if (e.type == SDL_QUIT) {
+				quit = true;
+			}
+		}
+	}
+
+	SDL_DestroyWindow(sdl_window);
+	SDL_Quit();
+}
---
+++ src/video_reader.hpp
@@ -0,0 +1,167 @@
+#ifndef VIDEO_READER
+#define VIDEO_READER
+
+#define SDL_MAIN_HANDLED
+extern "C" {
+#include <SDL2/SDL.h>
+#include <libavformat/avformat.h>
+#include <libavcodec/avcodec.h>
+#include <libswscale/swscale.h>
+}
+
+#include <memory>
+#include <chrono>
+
+struct VideoReader {
+	AVFormatContext* av_format_ctx = avformat_alloc_context();
+
+	AVRational time_base;
+
+	std::chrono::steady_clock::time_point first_frame_begin_time;
+	uint64_t frame_pst_microsec;
+	int8_t video_stream_index = -1;
+	AVCodecParameters* av_codec_params = nullptr;
+	AVCodec* av_codec = nullptr;
+	AVCodecContext* av_codec_ctx = nullptr;
+
+	AVFrame* av_frame = av_frame_alloc();
+	AVPacket* av_packet = av_packet_alloc();
+
+	SwsContext* sws_scaler_ctx = nullptr;
+
+	uint16_t width, height;
+	std::unique_ptr<uint8_t[]> frame_data = nullptr;
+
+	VideoReader(const char* filename) {
+
+		if (!av_frame || !av_packet) {
+			fprintf(stderr, "Couldn't allocate a packet or frame\n");
+			return;
+		}
+
+		if (!av_format_ctx) {
+			fprintf(stderr, "Couldn't create AVFormatContext\n");
+			return;
+		}
+
+		if (avformat_open_input(&av_format_ctx, filename, nullptr, nullptr) < 0) {
+			fprintf(stderr, "Couldn't open video file: %s\n", filename);
+			return;
+		}
+
+		for (auto i = 0; i < av_format_ctx->nb_streams; ++i) {
+			auto stream = av_format_ctx->streams[i];
+
+			av_codec_params = av_format_ctx->streams[i]->codecpar;
+			av_codec = avcodec_find_decoder(av_codec_params->codec_id);
+
+			if (!av_codec) {
+				continue;
+			}
+
+			if (av_codec_params->codec_type == AVMEDIA_TYPE_VIDEO) {
+				video_stream_index = i;
+				time_base = stream->time_base; 
+				break;
+			}
+		}
+
+		if (video_stream_index == -1) {
+			fprintf(stderr, "Couldn't find a video stream\n");
+			return;
+		}
+
+		av_codec_ctx = avcodec_alloc_context3(av_codec);
+
+		if (!av_codec_ctx) {
+			fprintf(stderr, "Couldn't allocate AVCodecContext\n");
+			return;
+		}
+
+		if (avcodec_parameters_to_context(av_codec_ctx, av_codec_params) < 0) {
+			fprintf(stderr, "Couldn't initialize AVCodecContext\n");
+			return;
+		}
+
+		if (avcodec_open2(av_codec_ctx, av_codec, nullptr) < 0) {
+			fprintf(stderr, "Couldn't open codec\n");
+			return;
+		}
+
+		width = av_codec_params->width;
+		height = av_codec_params->height;
+		frame_data = std::make_unique<uint8_t[]>(width * height * 4);
+	}
+
+	~VideoReader() {
+		if (av_format_ctx) {
+			avformat_close_input(&av_format_ctx);
+			avformat_free_context(av_format_ctx);
+		}
+		if (av_codec_ctx) avcodec_free_context(&av_codec_ctx);
+		if (av_packet) av_packet_free(&av_packet);
+		if (av_frame) av_frame_free(&av_frame);
+		if (sws_scaler_ctx) sws_freeContext(sws_scaler_ctx);
+	}
+
+	auto read_first_frame() {
+	}
+
+	auto read_single_frame() -> uint8_t* {
+		while (av_read_frame(av_format_ctx, av_packet) >= 0) {
+			if (av_packet->stream_index != video_stream_index) {
+				av_packet_unref(av_packet);
+				continue;
+			}
+
+			auto response = avcodec_send_packet(av_codec_ctx, av_packet);
+
+			if (response < 0) {
+				fprintf(stderr, "Failed to decode packet\n");
+				av_packet_unref(av_packet);
+				return nullptr;
+			}
+
+			response = avcodec_receive_frame(av_codec_ctx, av_frame);
+
+			if (response == AVERROR(EAGAIN) || response == AVERROR_EOF) {
+				av_packet_unref(av_packet);
+				continue;
+			}
+			else if (response < 0) {
+				fprintf(stderr, "Failed to receive frame\n");
+				av_packet_unref(av_packet);
+				return nullptr;
+			}
+
+			av_packet_unref(av_packet);
+			break;
+		}
+
+		frame_pst_microsec = (double) av_frame->pts * 1'000'000 * (double) time_base.num / (double) time_base.den; 
+
+		if (!sws_scaler_ctx) {
+			sws_scaler_ctx = sws_getContext(width, height, av_codec_ctx->pix_fmt,
+				width, height, AV_PIX_FMT_RGB0,
+				SWS_BILINEAR, nullptr, nullptr, nullptr);
+
+			if (!sws_scaler_ctx) {
+				fprintf(stderr, "Couldn't initialize sw scaler\n");
+				return nullptr;
+			}
+
+			first_frame_begin_time = std::chrono::steady_clock::now();
+		}
+
+		auto frame_data_rawptr = frame_data.get();
+
+		uint8_t* dest[4] = { frame_data_rawptr, nullptr, nullptr, nullptr };
+		const int dest_linesize[4] = { width * 4, 0, 0, 0 };
+		sws_scale(sws_scaler_ctx, av_frame->data, av_frame->linesize, 0, height, dest, dest_linesize);
+
+		return frame_data_rawptr;
+	}
+};
+
+
+#endif
