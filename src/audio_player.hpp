584ed67732a9299f9a58936daf3c72db4a0dfecc
CampoDiFiori <dudkopiotr@gmail.com>
2020-10-01 14:27:13 +0200

    Fix C-like syntax in audio_player.hpp

--- src/audio_player.hpp
+++ src/audio_player.hpp
@@ -1,4 +1,6 @@
-#include <soundio/soundio.h>
+extern "C" {
+    #include <soundio/soundio.h>
+}
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,13 +9,13 @@
 
 static const float PI = 3.1415926535f;
 static float seconds_offset = 0.0f;
-void write_callback(struct SoundIoOutStream *outstream,
+void write_callback(SoundIoOutStream *outstream,
         int frame_count_min, int frame_count_max)
 {
-    const struct SoundIoChannelLayout *layout = &outstream->layout;
+    const SoundIoChannelLayout *layout = &outstream->layout;
     float float_sample_rate = outstream->sample_rate;
     float seconds_per_frame = 1.0f / float_sample_rate;
-    struct SoundIoChannelArea *areas;
+    SoundIoChannelArea *areas;
     int frames_left = frame_count_max;
     int err;
 
@@ -28,6 +30,7 @@ void write_callback(struct SoundIoOutStream *outstream,
         if (!frame_count)
             break;
 
+        
         float pitch = 440.0f;
         float radians_per_second = pitch * 2.0f * PI;
         for (int frame = 0; frame < frame_count; frame += 1) {
